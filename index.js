2b8cab6904c8ccef1454495b92c6775525bfeb03
Nuno Frade <135842438+pink-phantom@users.noreply.github.com>
2025-11-22 11:57:28 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var createScene = function (engine, canvas) {
 
     camera.attachControl(canvas, true);
 
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 
     var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 3, scene);
 
